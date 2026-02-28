2785f27ecaf3f5c06270bdf01dbcef962c7a3f9c
Ethan <ethanwelz@gmail.com>
2026-02-27 22:38:40 -0500

    concatenation

---
+++ Concatenation.sql
@@ -0,0 +1,11 @@
+SELECT
+	DISTINCT Country_name,
+    Time_period,
+    Est_population_in_millions,
+    CONCAT(
+    SUBSTRING(UPPER(IFNULL(Country_name, 'UNKNOWN')),1,4),
+    SUBSTRING(IFNULL(Time_period, 'UNKNOWN'),1,4),
+    SUBSTRING(IFNULL(Est_population_in_millions, 'UNKNOWN'),-7)
+    ) AS Country_id
+FROM
+	united_nations.access_to_basic_services;
